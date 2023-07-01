d4cf1930b58b646a3eb1648f209c9bac6268ce30
adipshr <adi.pshr@gmail.com>
2023-07-01 15:03:28 +0530

    complete exercise 1.9

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 export default function Statistics({ good, bad, neutral, all }) {
-  return (
-    <>
-      <h3>Statistics : </h3>
-      <p>Good : {good}</p>
-      <p>Neutral : {neutral}</p>
-      <p>Bad : {bad}</p>
-      <p>All : {all}</p>
-      <p>Average : {all / 3}</p>
-      <p>Positive : {(good / all) * 100} %</p>
-    </>
-  );
+  if (all < 1) {
+    return <h3>No feedback given</h3>;
+  } else
+    return (
+      <>
+        <h3>Statistics : </h3>
+        <p>Good : {good}</p>
+        <p>Neutral : {neutral}</p>
+        <p>Bad : {bad}</p>
+        <p>All : {all}</p>
+        <p>Average : {all / 3}</p>
+        <p>Positive : {(good / all) * 100} %</p>
+      </>
+    );
 }
