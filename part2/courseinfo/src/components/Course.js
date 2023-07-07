a561378292d4f64ca674cbbc6035a942acb1f995
adipshr <adi.pshr@gmail.com>
2023-07-07 18:37:01 +0530

    complete exercise 2.1

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+import Content from "./Content";
+
+export default function Course({ course }) {
+  return (
+    <>
+      <Header course={course} />
+      <Content course={course} />
+    </>
+  );
+}
